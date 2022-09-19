87bad250bb77b968981b43a9d0266f9993bf1c33
Isaías Subero <contactosubero@gmail.com>
2022-09-19 11:37:27 +0200

    BIENVENIDO20 ahora otorga 3 ebooks

--- promos/bienvenido.php
+++ promos/bienvenido.php
@@ -16,7 +16,7 @@ function welcome_cart_updated() {
         return;
     }
 
-    // Ebook
+    // Ebook 5 consejos para mejorar tu estilo de vida saludable
     $ebook_product_id = wp_get_environment_type() == 'local' ? 2827 : 4013;
     $ebook_cart_id = WC()->cart->generate_cart_id($ebook_product_id);
     $ebook_cart_item_key = WC()->cart->find_product_in_cart($ebook_cart_id);
@@ -35,6 +35,50 @@ function welcome_cart_updated() {
     } else {
         if ( $ebook_cart_item_key ) {
             WC()->cart->remove_cart_item( $ebook_cart_item_key );
+        }
+    }
+
+    // Ebook 30 días de ejercicios
+    $ebook_1_product_id = 4061;
+    $ebook_1_cart_id = WC()->cart->generate_cart_id($ebook_1_product_id);
+    $ebook_1_cart_item_key = WC()->cart->find_product_in_cart($ebook_1_cart_id);
+
+    if ( WC()->cart->has_discount( 'bienvenido20' ) ) {
+        // If promo product is already in cart, update quantity
+        if ( $ebook_1_cart_item_key ) {
+            $cart_item = WC()->cart->get_cart_item( $ebook_1_cart_item_key );
+
+            if ( $cart_item['quantity'] > 1 ) {
+                WC()->cart->set_quantity( $ebook_1_cart_item_key, 1 );
+            }
+        } else {
+            WC()->cart->add_to_cart( $ebook_1_product_id, 1 );
+        }
+    } else {
+        if ( $ebook_1_cart_item_key ) {
+            WC()->cart->remove_cart_item( $ebook_1_cart_item_key );
+        }
+    }
+
+    // Ebook Lista de mercado saludble
+    $ebook_2_product_id = 4066;
+    $ebook_2_cart_id = WC()->cart->generate_cart_id($ebook_2_product_id);
+    $ebook_2_cart_item_key = WC()->cart->find_product_in_cart($ebook_2_cart_id);
+
+    if ( WC()->cart->has_discount( 'bienvenido20' ) ) {
+        // If promo product is already in cart, update quantity
+        if ( $ebook_2_cart_item_key ) {
+            $cart_item = WC()->cart->get_cart_item( $ebook_2_cart_item_key );
+
+            if ( $cart_item['quantity'] > 1 ) {
+                WC()->cart->set_quantity( $ebook_2_cart_item_key, 1 );
+            }
+        } else {
+            WC()->cart->add_to_cart( $ebook_2_product_id, 1 );
+        }
+    } else {
+        if ( $ebook_2_cart_item_key ) {
+            WC()->cart->remove_cart_item( $ebook_2_cart_item_key );
         }
     }
 }
